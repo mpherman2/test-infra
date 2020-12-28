730ed716c92d0a3668ff5cbdcd5bbc9539ebbbb0
mpherman2 <mpherman@google.com>
2020-12-28 11:42:56 -0800

    Update gcr.io/k8s-prow/, gcr.io/k8s-staging-boskos/, gcr.io/k8s-testimages/ images as necessary

--- experiment/autobumper/bumper/bumper.go
+++ experiment/autobumper/bumper/bumper.go
@@ -280,7 +280,7 @@ func Run(o *Options) error {
 			}
 		}
 
-		remoteBranch := "autobump-canary-test"
+		remoteBranch := "autobump-canary-test-2"
 		stdout := HideSecretsWriter{Delegate: os.Stdout, Censor: &sa}
 		stderr := HideSecretsWriter{Delegate: os.Stderr, Censor: &sa}
 		if err := MakeGitCommit(fmt.Sprintf("git@github.com:%s/%s.git", o.GitHubLogin, o.RemoteName), remoteBranch, o.GitName, o.GitEmail, o.Prefixes, stdout, stderr, versions); err != nil {
